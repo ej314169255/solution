1c624ff2c0accce72de45da619774b0424ae7c0f
ej314169255 <107900694+ej314169255@users.noreply.github.com>
2022-10-23 21:29:56 +0800

    Update sum_line.cpp

--- sum_line.cpp
+++ sum_line.cpp
@@ -5,10 +5,9 @@
 using namespace std;
 #define NUM_THREADS 2
 
-void sumFunction(unsigned int pos, unsigned long long first, unsigned long long last, unsigned long long SumArray)
+void sumFunction(unsigned int pos, unsigned long long first, unsigned long long last, unsigned long long *sumArray)
 {
     unsigned long long sum = 0;
-    unsigned long long sumArray;
     for (unsigned long long i = first; i <= last; ++i)
     {
         sum += i;
@@ -19,37 +18,59 @@ void sumFunction(unsigned int pos, unsigned long long first, unsigned long long 
 int main()
 {
     std::thread threads[NUM_THREADS];
-    unsigned long long sumArray[NUM_THREADS] = {0};
-    unsigned long long sum1 = 0, sum2 = 0, N = 10000;
+    unsigned long long sumArray[NUM_THREADS] = { 0 };
+    unsigned long long N = 10000000000;
     unsigned long long step = N / NUM_THREADS;
 
-    std::cout << "Start multithread calculation" << std::endl;
-    auto start = std::chrono::system_clock::now();
-    for (int i = 0; i < NUM_THREADS; i++) {
-        threads[i] = std::thread(sumFunction, i, i * step + 1, (i + 1) * step, sumArray);
-    }
+    int cur_iteration = 0;
+    int num_iteration = 9;
+    int sum_time = 0;
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        threads[i].join();
-        std::cout << "Thread " << i << "end calculation " << std::endl;
-    }
-    for (int i = 0; i < NUM_THREADS; i++) {
-        sum1 += sumArray[i];
-    }
-    auto end = std::chrono::system_clock::now();
-    auto elapsed1 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "End multithread calculation..." << std::endl;
-    std::cout << "Sum 1 = " << sum1 << " with time: " << elapsed1 << " msec" << std::endl;
-    std::cout << "Start single thread calculation..." << std::endl;
+    std::cout << "Start multithread thread calculation 10 counts" << std::endl;
+    while (cur_iteration < num_iteration + 1)
+    {
+        cur_iteration++;
+        unsigned long long sum1 = 0;
+        auto start = std::chrono::system_clock::now();
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i] = std::thread(sumFunction, i, i * step + 1, (i + 1) * step, sumArray);
+        }
 
-    start = std::chrono::system_clock::now();
-
-    for (unsigned long long y = 1; y <= N; y++) {
-        sum2 += y;
+        for (int i = 0; i < NUM_THREADS; i++) {
+            threads[i].join();
+            //std::cout << "Thread " << i << "end calculation " << std::endl;
+        }
+        for (int i = 0; i < NUM_THREADS; i++) {
+            sum1 += sumArray[i];
+        }
+        auto end = std::chrono::system_clock::now();
+        auto el_single = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "number calculation is " << cur_iteration << "................ " << el_single << std::endl;
+        sum_time += el_single;
     }
-    end = std::chrono::system_clock::now();
-    auto elapsed2 = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "End single thread calculation..." << std::endl;
-    std::cout << "Sum 2 = " << sum2 << " with time: " << elapsed2 << " msec" << std::endl;
+    std::cout << "End multithread calculation with avarege time: " << sum_time / (num_iteration + 1) << " milliseconds" << std::endl;
+
+    cur_iteration = 0;
+    num_iteration = 9;
+    sum_time = 0;
+
+    std::cout << "Start single calculation 10 counts" << std::endl;
+    while (cur_iteration < num_iteration + 1)
+    {
+        cur_iteration++;
+        unsigned long long sum2 = 0;
+        auto start = std::chrono::system_clock::now();
+        for (uint64_t y = 1; y <= N; y++) {
+            sum2 += y;
+        }
+        auto end = std::chrono::system_clock::now();
+        auto el_single = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "number calculation is " << cur_iteration << "................ " << el_single << std::endl;
+        sum_time += el_single;
+    }
+    std::cout << "End single calculation with avarege time: " << sum_time / (num_iteration + 1) << " milliseconds" << std::endl;
+
+    auto start2 = std::chrono::system_clock::now();
+    
     return 0;
 }
